42cda9071f08745e874ecd40f89106e5dac78ee0
error418 <error418@hotmail.com>
2018-11-04 13:50:54 -0800

    Initial commit

---
+++ CPPConstantObjects/Source.cpp
@@ -0,0 +1,23 @@
+// CPP Constant Objects - by Ray
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+	cin.get();
+}
