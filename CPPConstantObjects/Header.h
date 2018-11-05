626eb115ed863e3a29156bfa1f5c665fdbb37d78
error418 <error418@hotmail.com>
2018-11-05 05:33:06 -0800

    Mid commit

---
+++ CPPConstantObjects/Header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int n = 2;
+int	p = 8;
+int	e = 1;
+int	i = 1;
--- CPPConstantObjects/Source.cpp
+++ CPPConstantObjects/Source.cpp
@@ -4,20 +4,60 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 using std::cout;
 using std::endl;
 
-int main()
-{
+
+/*First example
+In the previous module, you created a for loop to calculate a base raised to an exponent. In this peer review, you are going to put that code into a function and create two more functions. Paste your code in the response section below that satisfies these requirements:
+
+Create a function called pow that accepts two integers that represent the base and the exponent. The exponent should not permit negative values.
+Use the code you created in the previous module for some of this functionality but add additional code
+The additional code should make use of an if statement to check the exponent value. If it is 0, then immediately return 1 otherwise calculate the power and return the result.
+Show sample code that will call the function and get the result. */
+
+int n = 2;
+int	p = 8;
+int	e = 1;
+int	i = 1;
+
+int pow ();
+	{
+		for (i = 1; i <= p; i++)
+		{
+			e = i * n;
+			if (e == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				if (e = > 1)
+				{
+					cout << e << endl;
+				}
+			}
+		}
+	}
 
 
+/*Function for how to find Sin? 
+Sin = Opposite / Hypetenuse*/
 
+/*For the second function you will compute the sine of an angle. Your function should accept the opposite and hypotenuse lengths and return the sine value. The formula for sine is: 
+sin = opposite / hypotenuse
+where / is used to denote division
+Ensure that you paste the code for the function as well as the code that calls the function and assigns the value to a variable.*/
 
+	int opposite = 5;
+	int hypotenuse = 2;
 
+	
+	int Triangle()
+	{
+		int sin = (opposite / hypotenuse);
 
-
-
-
-	cin.get();
-}
+			cout << sin << endl;
+	}
